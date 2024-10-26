620bc1a697a03602976d428eaf237838d8dea0dc
Gokulggll <gokulg7306@gmail.com>
2024-10-26 19:05:34 +0530

    staring project

---
+++ src/componets/Header.js
@@ -0,0 +1,12 @@
+import '../style/Header.css'
+
+const Header=()=>{
+    return(
+        <div className='header_css'>
+          <h1>TodoList and Notes</h1>  
+        </div>
+
+    )
+}
+
+export default Header;
